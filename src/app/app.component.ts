c21d4e73724bd0b4c681a2ae09f623a56b69a78d
alankarapte <alankarapte7588@gmail.com>
2021-10-07 01:21:30 +0530

    css changed to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   pdfForm: FormGroup;
